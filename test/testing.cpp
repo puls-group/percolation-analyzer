edb5dd1f290f718cef9a97526268ded87a709582
Reshief <reshief@gmx.net>
2021-07-22 12:20:09 +0200

    Add test for memory allocation check

--- test/testing.cpp
+++ test/testing.cpp
@@ -2,6 +2,26 @@
 #include "catch.hpp"
 #include "percolation-detection.hpp"
 
-TEST_CASE( "The graph should allocate memory for entries", "[graph allocation]" ) {
+using namespace percolation;
 
+TEST_CASE( "The graph should automatically allocate memory for entries", "[graph allocation]" ) {
+    PercolationGraph graph;
+
+    TranslationVector trans1, trans2, trans3;
+
+    trans1.vec[0] = 1;
+    trans1.vec[1] = 0;
+    trans1.vec[2] = 0;
+
+    trans2.vec[0] = 0;
+    trans2.vec[1] = 1;
+    trans2.vec[2] = 0;
+
+    trans3.vec[0] = 0;
+    trans3.vec[1] = 0;
+    trans3.vec[2] = 1;
+
+    graph.add_edge(0, 1, trans1);
+    graph.add_edge(0, 2, trans1);    
+    graph.add_edge(0, 2, trans1);    
 }
