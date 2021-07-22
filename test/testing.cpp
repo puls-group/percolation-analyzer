2d803a45c836dbb363835ea77494f83459b82520
Reshief <reshief@gmx.net>
2021-07-22 12:05:47 +0200

    Add test skeleton

---
+++ test/testing.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "percolation-detection.hpp"
+
+TEST_CASE( "The graph should allocate memory for entries", "[graph allocation]" ) {
+
+}
