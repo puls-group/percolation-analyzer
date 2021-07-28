acba1e88076d4d8b77a41ae2eb1ee8e442fcb2cd
Reshief <reshief@gmx.net>
2021-07-28 17:00:10 +0200

    Implement graph conversion

--- src/molecular_graph.cpp
+++ src/molecular_graph.cpp
@@ -52,6 +52,72 @@ namespace mol
     percolation::PercolationGraph MolecularGraph::get_percolation_graph() const
     {
         percolation::PercolationGraph res;
+        // Resize the percolation graph appropriately
+        res.reserve_vertices(n_atoms);
+
+
+        // Transform all positions into normalized basis components
+        // This is equivalent to moving them all into one pbc cell and transforming the coordinates
+        // into cuboid shape, which makes everything simpler.
+        std::vector<vec<graph_precision_type>> normalized_positions(n_atoms);
+        for (size_t base = 0; base < n_atoms; base++)
+        {
+            normalized_positions[base] = normalize_basis_coefficients(decompose(atom_positions[base], triclinic_basis));
+        }
+
+        // Parse the edges to be added:
+        for (size_t base = 0; base < n_atoms; base++)
+        {
+            const vec<graph_precision_type> &b_pos = normalized_positions[base];
+            for (size_t n = 0; n < bonds[base].size(); n++)
+            {
+                size_t head = bonds[base][n];
+                if (head < base)
+                {
+                    // Both directions of the edge were created, we only need to consider one.
+                    continue;
+                }
+
+                const vec<graph_precision_type> &h_pos = normalized_positions[base];
+
+                // Let us build the correct translation vector
+                percolation::TranslationVector trans;
+
+                for (size_t dim = 0; dim < vector_space_dimension; dim++)
+                {
+                    if (b_pos[dim] < h_pos[dim])
+                    {
+                        // The base lies below the head in the cell
+                        if (h_pos[dim] - b_pos[dim] > 0.5)
+                        {
+                            // The shorter connection intersects the cell boundary going down from the base
+                            trans[dim] = -1;
+                        }
+                        else
+                        {
+                            // The connection is within the cell
+                            trans[dim] = 0;
+                        }
+                    }
+                    else
+                    {
+                        // Need to check edge crossing the other way around
+                        // The base lies below the head in the cell
+                        if (b_pos[dim] - h_pos[dim] > 0.5)
+                        {
+                            // The shorter connection intersects the cell boundary going up from the base
+                            trans[dim] = +1;
+                        }
+                        else
+                        {
+                            // The connection is within the cell
+                            trans[dim] = 0;
+                        }
+                    }
+                }
+                res.add_edge(base, head, trans);
+            }
+        }
 
         return res;
     }
